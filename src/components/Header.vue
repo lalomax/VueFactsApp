34e626f22066edb15b23c374b57358f1acd7e9d2
Lalomax <lalomax365@gmail.com>
2025-11-17 19:32:04 -0400

    adding vue image logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,14 +2,12 @@
 </script>
 
 <template>
-    <header>
-        <!--
-            This image has been commented out for now as it will break
-            until you've learnt how to work with images!
-            <img src="logo_vue_300px.png" alt="vue logo" />
-            -->
-        <h1>Hi, I'm <span class="highlight">Vue</span></h1>
-    </header>
+  <header>
+
+    <img src="@/assets/logo_vue_300px.png" alt="vue logo" />
+
+    <h1>Hi, I'm <span class="highlight">Vue</span></h1>
+  </header>
 </template>
 
 <style scoped>
@@ -17,10 +15,12 @@
 header img {
   height: 150px;
 }
+
 h1 {
   font-size: 2rem;
   color: #34495e;
   margin-top: 10px;
 }
+
 /**********/
 </style>
